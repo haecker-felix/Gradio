99b0d2a5b0d5c9d118a71da756072100c8cdcfe3
Felix Häcker <haecker.felix1207@gmail.com>
2016-05-10 14:32:33 +0200

    init

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/haecker-felix/Schreibtisch/Gradio
-# Build directory: /home/haecker-felix/Schreibtisch/Gradio/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
