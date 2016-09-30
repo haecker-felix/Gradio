d4acbf467a62706eed72fb3f92d3818d3615cb85
Häcker Felix <haecker.felix1207@gmail.com>
2016-09-30 19:31:33 +0200

    Update git.mk

--- git.mk
+++ git.mk
@@ -194,28 +194,6 @@ $(srcdir)/.gitignore: Makefile.am $(top_srcdir)/git.mk
 				$(gsettings__enum_file) \
 			; do echo "/$$x"; done; \
 		fi; \
-		if test -f $(srcdir)/po/Makefile.in.in; then \
-			for x in \
-				po/Makefile.in.in \
-				po/Makefile.in.in~ \
-				po/Makefile.in \
-				po/Makefile \
-				po/Makevars.template \
-				po/POTFILES \
-				po/Rules-quot \
-				po/stamp-it \
-				po/.intltool-merge-cache \
-				"po/*.gmo" \
-				"po/*.header" \
-				"po/*.mo" \
-				"po/*.sed" \
-				"po/*.sin" \
-				po/$(GETTEXT_PACKAGE).pot \
-				intltool-extract.in \
-				intltool-merge.in \
-				intltool-update.in \
-			; do echo "/$$x"; done; \
-		fi; \
 		if test -f $(srcdir)/configure; then \
 			for x in \
 				autom4te.cache \
