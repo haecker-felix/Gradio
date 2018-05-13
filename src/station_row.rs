5c176747bad74990c640c921b7071c31f35f75d3
Felix Häcker <haecker.felix1207@gmail.com>
2018-05-13 19:49:15 +0200

    Reduced window size, library list is now scrollable.

--- src/station_row.rs
+++ src/station_row.rs
@@ -5,19 +5,19 @@ use rustio::station::Station;
 
 pub struct StationRow {
     pub container: gtk::Box,
+    pub play_button: gtk::Button,
     builder: gtk::Builder,
 }
 
 impl StationRow {
      pub fn new(station: &Station) -> Self {
          let builder = gtk::Builder::new_from_string(include_str!("station_row.ui"));
-         let container: gtk::Box = builder.get_object("station_row").unwrap();
 
+         let container: gtk::Box = builder.get_object("station_row").unwrap();
+         let play_button: gtk::Button = builder.get_object("play_button").unwrap();
          let station_label: gtk::Label = builder.get_object("station_label").unwrap();
          station_label.set_text(&station.name);
 
-         container.add(&station_label);
-
-         Self {container,builder}
+         Self {container, play_button, builder}
     }
 }
