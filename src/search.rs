74cd13a3aa627810afd2af07166ea8b5f24d4e2b
Felix Häcker <haecker.felix1207@gmail.com>
2018-12-09 11:15:32 +0100

    add page: add new bottom toolbar

--- src/search.rs
+++ src/search.rs
@@ -16,23 +16,6 @@ impl Search {
     pub fn new(sender: Sender<Action>) -> Self {
         let builder = gtk::Builder::new_from_resource("/de/haeckerfelix/Gradio/gtk/search.ui");
         let widget: gtk::Box = builder.get_object("search").unwrap();
-        let content_box: gtk::Box = builder.get_object("content_box").unwrap();
-        let sidebar_box: gtk::Box = builder.get_object("sidebar_box").unwrap();
-        let stack_box: gtk::Box = builder.get_object("stack_box").unwrap();
-
-        // Setup HdyLeaflet
-        let leaflet = Leaflet::new();
-        leaflet.set_mode_transition_type(LeafletModeTransitionType::Slide);
-        leaflet.set_child_transition_type(LeafletChildTransitionType::Slide);
-        content_box.add(&leaflet);
-
-        let leaflet = leaflet.upcast::<gtk::Widget>(); // See https://gitlab.gnome.org/World/podcasts/blob/master/podcasts-gtk/src/widgets/home_view.rs#L64
-        let leaflet = leaflet.downcast::<gtk::Container>().unwrap();
-        leaflet.add(&sidebar_box);
-        leaflet.add(&stack_box);
-        leaflet.show();
-        leaflet.set_vexpand(true);
-        //leaflet.add(&library.collection_listbox.widget);
 
         let search = Self { widget, builder, sender };
 
@@ -40,5 +23,10 @@ impl Search {
         search
     }
 
-    fn setup_signals(&self) {}
+    fn setup_signals(&self) {
+        let search_entry: gtk::SearchEntry = self.builder.get_object("search_entry").unwrap();
+        search_entry.connect_search_changed(move|_|{
+            debug!("search changed");
+        });
+    }
 }
