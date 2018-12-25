2fc5d3ee5837dc449e5be105aa60b1e4d1606c83
Felix Häcker <felix.haecker@posteo.de>
2018-12-25 11:53:46 +0100

    implement new gstreamer pipeline to record songs. Basics are working now

--- src/recorder.rs
+++ src/recorder.rs
@@ -3,46 +3,64 @@ use gstreamer_pbutils::prelude::*;
 use gtk::prelude::*;
 use mpris_player::{Metadata, MprisPlayer, OrgMprisMediaPlayer2Player, PlaybackStatus};
 use rustio::{Client, Station};
+use gstreamer::ElementExt;
+use gstreamer::BinExt;
 
 use std::cell::Cell;
 use std::cell::RefCell;
 use std::rc::Rc;
-use std::sync::mpsc::Sender;
 use std::sync::Arc;
+use std::sync::Mutex;
+use std::sync::mpsc::Sender;
 use std::thread;
 
 use crate::app::{Action, AppInfo};
 
-pub struct Recorder {
-    pub widget: gtk::Box,
-    current_station: Cell<Option<Station>>,
-    current_song: Rc<RefCell<String>>,
+/////////////////////////////////////////////////////////////////////////////////////////////
+//                                                                                         //
+//  -> Gstreamer Pipeline                                                                  //
+//    --------------      --------------      -----------      -------      ------------   //
+//   | uridecodebin | -> | audioconvert | -> | vorbisenc | -> | queue | -> | muxsinkbin |  //
+//    --------------      --------------      -----------      -------      ------------   //
+//                                                                                         //
+//  -> muxsinkbin:  (gstreamer::Bin)                                                       //
+//    --------------------------------------------                                         //
+//   |                  --------      ----------  |                                        //
+//   | ( ghostpad ) -> | oggmux | -> | filesink | |                                        //
+//   |                  --------      ----------  |                                        //
+//    --------------------------------------------                                         //
+//                                                                                         //
+/////////////////////////////////////////////////////////////////////////////////////////////
 
-    pipeline: gstreamer::Pipeline,
-    uridecodebin: gstreamer::Element,
-    audioconvert: gstreamer::Element,
-    vorbisenc: gstreamer::Element,
-    oggmux: gstreamer::Element,
-    filesink: gstreamer::Element,
+pub struct GstBackend{
+    pub pipeline: gstreamer::Pipeline,
 
-    builder: gtk::Builder,
-    sender: Sender<Action>,
+    pub uridecodebin: gstreamer::Element,
+    pub audioconvert: gstreamer::Element,
+    pub vorbisenc: gstreamer::Element,
+    pub queue: gstreamer::Element,
+    pub muxsinkbin: Option<gstreamer::Bin>,
+
+    pub queue_srcpad: gstreamer::Pad,
+    pub queue_blockprobe_id: Option<gstreamer::PadProbeId>,
 }
 
-impl Recorder {
-    pub fn new(sender: Sender<Action>) -> Self {
-        let builder = gtk::Builder::new_from_resource("/de/haeckerfelix/Gradio/gtk/recorder.ui");
-        let widget: gtk::Box = builder.get_object("recorder").unwrap();
-        let current_station = Cell::new(None);
-        let current_song = Rc::new(RefCell::new(String::new()));
+impl GstBackend{
+    pub fn new() -> Self{
+        // create gstreamer pipeline
+        let pipeline = gstreamer::Pipeline::new("recorder_pipeline");
 
-        let pipeline = gstreamer::Pipeline::new(None);
+        // create pipeline elements
         let uridecodebin = gstreamer::ElementFactory::make("uridecodebin", "uridecodebin").unwrap();
         let audioconvert = gstreamer::ElementFactory::make("audioconvert", "audioconvert").unwrap();
         let vorbisenc = gstreamer::ElementFactory::make("vorbisenc", "vorbisenc").unwrap();
-        let oggmux = gstreamer::ElementFactory::make("oggmux", "oggmux").unwrap();
-        let filesink = gstreamer::ElementFactory::make("filesink", "filesink").unwrap();
+        let queue = gstreamer::ElementFactory::make("queue", "queue").unwrap();
 
+        // link pipeline elements
+        pipeline.add_many(&[&uridecodebin, &audioconvert, &vorbisenc, &queue]).unwrap();
+        gstreamer::Element::link_many(&[&audioconvert, &vorbisenc, &queue]).unwrap();
+
+        // dynamically link uridecodebin element with audioconvert element
         let convert = audioconvert.clone();
         uridecodebin.connect_pad_added(move |uridecodebin, src_pad|{
             let sink_pad = convert.get_static_pad("sink").expect("Failed to get static sink pad from convert");
@@ -60,20 +78,111 @@ impl Recorder {
             }
         });
 
-        pipeline.add_many(&[&uridecodebin, &audioconvert, &vorbisenc, &oggmux, &filesink]).unwrap();
-        gstreamer::Element::link_many(&[&audioconvert, &vorbisenc, &oggmux, &filesink]).unwrap();
-        filesink.set_property("location", &"/dev/null").unwrap();
+        let queue_srcpad = queue.get_static_pad("src").unwrap();
+
+        let mut pipeline = Self{
+            pipeline,
+            uridecodebin,
+            audioconvert,
+            vorbisenc,
+            queue,
+            muxsinkbin: None,
+            queue_srcpad,
+            queue_blockprobe_id: None,
+        };
+
+        pipeline.create_muxsinkbin("/dev/null");
+        pipeline
+    }
+
+    pub fn new_source_uri(&mut self, source: &str){
+        debug!("Stop pipeline...");
+        let _ = self.pipeline.set_state(gstreamer::State::Null);
+
+        debug!("Set new source uri...");
+        self.uridecodebin.set_property("uri", &source).unwrap();
+
+        debug!("Start pipeline...");
+        let _ = self.pipeline.set_state(gstreamer::State::Playing);
+    }
+
+    pub fn new_filesink_location(&mut self, location: &str){
+        debug!("Update filesink location to \"{}\"...", location);
+
+        debug!("Destroy old muxsinkbin");
+        let muxsinkbin = self.muxsinkbin.take().unwrap();
+        muxsinkbin.set_state(gstreamer::State::Null);
+        self.pipeline.remove(&muxsinkbin);
+
+        debug!("Create new muxsinkbin");
+        self.create_muxsinkbin(location);
+
+        debug!("Remove block probe...");
+        self.queue_srcpad.remove_probe(self.queue_blockprobe_id.take().unwrap());
+    }
+
+    fn create_muxsinkbin(&mut self, location: &str){
+        // Create oggmux
+        let oggmux = gstreamer::ElementFactory::make("oggmux", "oggmux").unwrap();
+
+        // Create filesink
+        let filesink = gstreamer::ElementFactory::make("filesink", "filesink").unwrap();
+        filesink.set_property("location", &location).unwrap();
+
+        // Create bin
+        let bin = gstreamer::Bin::new("bin");
+        bin.set_property("message-forward", &true).unwrap();
+
+        // Add elements to bin and link them
+        bin.add(&oggmux).unwrap();
+        bin.add(&filesink).unwrap();
+        gstreamer::Element::link_many(&[&oggmux, &filesink]).unwrap();
+
+        // Add bin to pipeline
+        self.pipeline.add(&bin).unwrap();
+
+        // Link queue src pad with oggmux sinkpad using a ghostpad
+        let sinkpad_template = oggmux.get_pad_template("audio_%u").unwrap();
+        let oggmux_sinkpad = oggmux.request_pad(&sinkpad_template, None, None).unwrap();
+
+        let ghostpad = gstreamer::GhostPad::new("sink", &oggmux_sinkpad).unwrap();
+        bin.add_pad(&ghostpad).unwrap();
+        bin.sync_state_with_parent();
+
+        if self.queue_srcpad.link(&ghostpad) != gstreamer::PadLinkReturn::Ok {
+            warn!("Queue src pad cannot linked to oggmux sinkpad");
+        }
+
+        self.muxsinkbin = Some(bin);
+    }
+}
+
+
+pub struct Recorder {
+    pub widget: gtk::Box,
+    current_station: Cell<Option<Station>>,
+    current_song: Rc<RefCell<String>>,
+    gst_backend: Arc<Mutex<GstBackend>>,
+
+    builder: gtk::Builder,
+    sender: Sender<Action>,
+}
+
+impl Recorder {
+    pub fn new(sender: Sender<Action>) -> Self {
+        let builder = gtk::Builder::new_from_resource("/de/haeckerfelix/Gradio/gtk/recorder.ui");
+        let widget: gtk::Box = builder.get_object("recorder").unwrap();
+        let current_station = Cell::new(None);
+        let current_song = Rc::new(RefCell::new("".to_string()));
+
+        // create gstreamer pipeline
+        let gst_backend = Arc::new(Mutex::new(GstBackend::new()));
 
         let recorder = Self {
             widget,
             current_station,
             current_song,
-            pipeline,
-            uridecodebin,
-            audioconvert,
-            vorbisenc,
-            oggmux,
-            filesink,
+            gst_backend,
             builder,
             sender,
         };
@@ -84,57 +193,67 @@ impl Recorder {
 
     pub fn set_station(&self, station: Station) {
         self.current_station.set(Some(station.clone()));
-        let pipeline = self.pipeline.clone();
-        let _ = pipeline.set_state(gstreamer::State::Paused);
+        let _ = self.gst_backend.lock().unwrap().pipeline.set_state(gstreamer::State::Paused);
 
-        let uridecodebin = self.uridecodebin.clone();
-        let filesink = self.filesink.clone();
+        let gst_backend = self.gst_backend.clone();
         thread::spawn(move || {
             let mut client = Client::new("http://www.radio-browser.info");
             let station_url = client.get_playable_station_url(station).unwrap();
-
-            let _ = pipeline.set_state(gstreamer::State::Null);
-            uridecodebin.set_property("uri", &station_url).unwrap();
-            filesink.set_property("location", &"/dev/null").unwrap();
-            let _ = pipeline.set_state(gstreamer::State::Playing);
+            debug!("new source uri to record: {}", station_url);
+            gst_backend.lock().unwrap().new_source_uri(&station_url)
         });
     }
 
-    fn parse_bus_message(message: &gstreamer::Message, pipeline: gstreamer::Pipeline, filesink: gstreamer::Element, current_song: Rc<RefCell<String>>) {
+    fn parse_bus_message(message: &gstreamer::Message, gstpipe: Arc<Mutex<GstBackend>>, current_song: Rc<RefCell<String>>) {
         match message.view() {
             gstreamer::MessageView::Tag(tag) => {
                 tag.get_tags().get::<gstreamer::tags::Title>().map(|title| {
+                    // Check if song have changed
                     if *current_song.borrow() != title.get().unwrap() {
                         *current_song.borrow_mut() = title.get().unwrap().to_string();
                         debug!("New song detected: {}", current_song.borrow());
-                        let path = &format!("{}/{}.ogg", glib::get_user_special_dir(glib::UserDirectory::Music).unwrap().to_str().unwrap(), title.get().unwrap());
-                        debug!("Save song: {}", path);
 
-                        // set state to 'null', so we can change filesink location
-                        let _ = pipeline.set_state(gstreamer::State::Null);
+                        debug!("Block the dataflow ...");
+                        let gstp = gstpipe.clone();
+                        let id = gstpipe.lock().unwrap().queue_srcpad.add_probe (gstreamer::PadProbeType::BLOCK_DOWNSTREAM, move|pad, info|{
+                            debug!("Pad is blocked now.");
 
-                        filesink.set_property("location", path);
+                            debug!("Push EOS into muxsinkbin sinkpad...");
+                            let sinkpad = gstp.lock().unwrap().muxsinkbin.clone().unwrap().get_static_pad("sink").unwrap();
+                            sinkpad.send_event(gstreamer::Event::new_eos().build());
 
-                        // start pipeline again
-                        let _ = pipeline.set_state(gstreamer::State::Playing);
+                            gstreamer::PadProbeReturn::Ok
+                        }).unwrap();
+
+                        // We need the padprobe id later to remove the block probe
+                        gstpipe.lock().unwrap().queue_blockprobe_id = Some(id);
                     }
                 });
-            }
+            },
+            gstreamer::MessageView::Element(element) => {
+                let structure = element.get_structure().unwrap();
+                if structure.get_name() == "GstBinForwarded" {
+                    let message: gstreamer::message::Message = structure.get("message").unwrap();
+                    if let gstreamer::MessageView::Eos(eos) = &message.view(){
+                        debug!("muxsinkbin got EOS...");
+                        let path = &format!("{}/{}.ogg", glib::get_user_special_dir(glib::UserDirectory::Music).unwrap().to_str().unwrap(), &*current_song.borrow());
+                        gstpipe.lock().unwrap().new_filesink_location(&path);
+                    }
+                }
+            },
             _ => (),
         };
     }
 
     fn setup_signals(&self) {
-        // new pipeline bus messages
-        let bus = self.pipeline.get_bus().expect("Unable to get pipeline bus");
-        let pipeline = self.pipeline.clone();
-        let filesink = self.filesink.clone();
+        let bus = self.gst_backend.lock().unwrap().pipeline.get_bus().expect("Unable to get pipeline bus");
+        let gst_backend = self.gst_backend.clone();
         let current_song = self.current_song.clone();
         gtk::timeout_add(250, move || {
             while bus.have_pending() {
                 bus.pop().map(|message| {
                     //debug!("new message {:?}", message);
-                    Self::parse_bus_message(&message, pipeline.clone(), filesink.clone(), current_song.clone());
+                    Self::parse_bus_message(&message, gst_backend.clone(), current_song.clone());
                 });
             }
             Continue(true)
